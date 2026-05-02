88153bed2d3a88db6b6a6712427add1a7ff2509d
Gabriel Morais de Jesus <gabrielmoraisdejesus1@gmail.com>
2026-05-02 19:52:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,85 @@
 var map = L.map('map').setView([-23.176, -47.281], 13);
+var icones = {
+    futebol: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41]
+    }),
+    basquete: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41]
+    }),
+
+    tenis: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41]
+    }),
+
+    padrao: L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/252/252025.png',
+        iconSize: [25, 41],
+        iconAnchor: [15, 30]
+    }) 
+};
+
+const iconePreto = L.icon({ // icone preto para localizçao do usuario
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-black.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+});
+
+map.locate({ setView: true, maxZoom: 16 }); // pegar localizaçao do usuario 
+function onLocationFound(e) {
+  const radius = e.accuracy;
+
+  L.marker(e.latlng, { icon: iconePreto }) 
+    .addTo(map)
+    .bindPopup("Você está aqui")
+    .openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on("locationfound", onLocationFound);
+
+map.on("locationerror", function() {
+  alert("Não foi possível obter sua localização");
+});
+
+let count_futebol = 0;
+let count_basquete = 0;
+let count_tenis = 0;
+let count_outros = 0;
+
+function escolherIcone(tipo){ // funçaõ que retorna o icone certo 
+    if (!tipo) return null;
+
+    tipo = tipo.toLowerCase();
+
+    if (tipo.includes("futebol")){
+        count_futebol++;
+        return icones.futebol; 
+    } 
+    if (tipo.includes("basquete")){
+        count_basquete++;
+        return icones.basquete; 
+    }
+    if (tipo.includes("tênis")){
+        count_tenis++;
+        return icones.tenis; 
+    }
+    count_outros++;
+    return null;
+}
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Carrega o GeoJSON
+// Carrega o GeoJSON da cidade de Salto
 fetch('saltoGJON.geojson')
     .then(res => res.json())
     .then(data => {
@@ -16,55 +91,37 @@ fetch('saltoGJON.geojson')
     map.setMaxBounds(cityLayer.getBounds());
     map.options.maxBoundsViscosity = 1.0; // impede o usuario de sair do limite
     });
-fetch('quadras.geojson')
+ 
+fetch('dados_com_bairro.json') // quadra + bairro 
 .then(res => res.json())
 .then(data => {
-
-    L.geoJSON(data, {
-        style: function(feature) {
-            return {
-                color: 'green',
-                weight: 2,
-                fillOpacity: 0.5
-            };
-        },
-
-        onEachFeature: function(feature, layer) {
-            let esporte = feature.properties.sport || "Esporte";
-            layer.bindPopup("<b>Quadra</b><br>" + esporte);
-        }
-
-    }).addTo(map);
-});
-fetch('quadras.geojson')
-.then(res => res.json())
-.then(data => {
-
-    L.geoJSON(data, {
-
-        style: {
-            color: 'green',
-            fillOpacity: 0.3
-        },
-
-        onEachFeature: function(feature, layer) {
-
-            let esporte = feature.properties.sport || "Esporte";
-
-            layer.bindPopup("<b>Quadra</b><br>" + esporte);
-
-            // cria marcador no centro
-            let center = layer.getBounds().getCenter();
-
-            L.marker(center)
+    data.forEach(item => {
+        let icone = escolherIcone(item.tipo);
+        if (icone) {
+            L.marker([item.latitude, item.longitude], { icon: icone })
                 .addTo(map)
-                .bindPopup("Centro da quadra: " + esporte);
+                .bindPopup(
+                    `<b>${item.categoria}</b><br>
+                     Esporte: ${item.tipo}<br>
+                     Bairro: ${item.bairro}`
+                );
+        } else {
+            L.marker([item.latitude, item.longitude])
+                .addTo(map)
+                .bindPopup(
+                    `<b>${item.categoria}</b><br>
+                     Esporte: ${item.tipo}<br>
+                     Bairro: ${item.bairro}`
+                );
         }
-
-    }).addTo(map);
+    });
+    console.log("futebol -> ", count_futebol);
+    console.log("basquete ->", count_basquete);
+    console.log("tenis ->", count_tenis);
+    console.log("outros ->", count_outros);
 });
 
-fetch('praca.geojson') // mapa da cidade de salto-SP
+fetch('praca.geojson') // mapa da praças de salto-SP
 .then(res => res.json())
 .then(data => {
 
@@ -75,11 +132,8 @@ fetch('praca.geojson') // mapa da cidade de salto-SP
             fillOpacity: 0.3
         },
         onEachFeature: function(feature, layer) {
-
             let nome = feature.properties.name || "Praça";
-
             layer.bindPopup("<b>" + nome + "</b>");
         }
     }).addTo(map);
-
 });
