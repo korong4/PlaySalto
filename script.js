5da0218841b14f187aba5aa4880de0d596dab8d1
Gabriel Morais de Jesus <gabrielmoraisdejesus1@gmail.com>
2026-05-12 17:34:44 -0300

    mudança de para ver as coordenadas

--- script.js
+++ script.js
@@ -112,7 +112,8 @@ fetch('dados_com_bairro.json') // quadra + bairro
                 .addTo(map)
                 .bindPopup(
                     `<b>${item.categoria}</b><br>
-                     <b>${item.latitude,item.longitude} </b> <br>
+                     <b>${item.latitude} </b> <br>
+                     <b>${item.longitude} </b> <br>
                      Esporte: ${item.tipo}<br>
                      Bairro: ${item.bairro}`
                 );
